7a83efa7b7ec27af4485965c588d0dc51de55a43
pchoum <prgrela@gmail.com>
2017-05-10 12:15:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,10 +33,16 @@ $( document ).ready( function() {
   //Set Map
   function initialize() {
       var myLatlng = new google.maps.LatLng(51.0516843,3.7132496);
-
-      var roomerLatLng = new google.maps.LatLng(51.0516843,3.7132496);
-
-
+      var RoomerLatLng = new google.maps.LatLng(51.0423538,3.7364158);
+      var SaoLatLng = new google.maps.LatLng(51.0510729,3.7237078);
+      var AlixLatLng = new google.maps.LatLng(51.0396106,3.7143295);
+      var RoggeLatLng = new google.maps.LatLng(51.0522247,3.7205942);
+      var HaLatLng = new google.maps.LatLng(51.0498455,3.7203618);
+      var JansLatLng = new google.maps.LatLng(51.0554145,3.7180731);
+      var PainLatLng = new google.maps.LatLng(51.0490668,3.7248812);
+      var HinkLatLng = new google.maps.LatLng(51.0501473,3.7356745);
+      var SevensLatLng = new google.maps.LatLng(51.0520202,3.7242284);
+    
       var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
       var mapOptions = {
         zoom: 12,
@@ -51,7 +57,7 @@ $( document ).ready( function() {
 
 
 
-    /*  MARKER BLOCK*/
+    /*  TEMPLATE MARKER BLOCK */
 
     var contentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
     //Set window width + content
@@ -72,10 +78,197 @@ $( document ).ready( function() {
       infowindow.open(map,marker);
     });
 
+    /*  ROOMER BLOCK */
 
+    var roomerContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: roomerContentString,
+      maxWidth: 500
+    });
 
+    //Add Marker
+    var roomerMarker = new google.maps.Marker({
+      position: RoomerLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
 
+    google.maps.event.addListener(roomerMarker, 'click', function() {
+      infowindow.open(map,roomerMarker);
+    });
 
+    /*  SAO BLOCK */
+
+    var saoContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: saoContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var SaoMarker = new google.maps.Marker({
+      position: SaoLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(SaoMarker, 'click', function() {
+      infowindow.open(map,SaoMarker);
+    });
+
+    /*  ALIX BLOCK */
+
+    var alixContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: alixContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var AlixMarker = new google.maps.Marker({
+      position: AlixLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(AlixMarker, 'click', function() {
+      infowindow.open(map,AlixMarker);
+    });
+    
+    /*  ROGGE BLOCK */
+
+    var roggeContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: roggeContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var RoggeMarker = new google.maps.Marker({
+      position: RoggeLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(RoggeMarker, 'click', function() {
+      infowindow.open(map,RoggeMarker);
+    });
+    
+    /*  HA BLOCK */
+
+    var haContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: haContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var HaMarker = new google.maps.Marker({
+      position: HaLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(HaMarker, 'click', function() {
+      infowindow.open(map,HaMarker);
+    });
+    
+    /*  JANS BLOCK */
+
+    var jansContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: jansContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var JansMarker = new google.maps.Marker({
+      position: JansLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(JansMarker, 'click', function() {
+      infowindow.open(map,JansMarker);
+    });
+    
+    /*  PAIN BLOCK */
+
+    var painContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: painContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var PainMarker = new google.maps.Marker({
+      position: PainLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(PainMarker, 'click', function() {
+      infowindow.open(map,PainMarker);
+    });
+    
+    /*  HINK BLOCK */
+
+    var hinkContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: hinkContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var HinkMarker = new google.maps.Marker({
+      position: HinkLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+
+    google.maps.event.addListener(HinkMarker, 'click', function() {
+      infowindow.open(map,HinkMarker);
+    });
+    
+    /*  SEVENS BLOCK */
+
+    var sevensContentString = 'Some <a href="http://www.youtube.com"> address </a> here..';
+    //Set window width + content
+    var infowindow = new google.maps.InfoWindow({
+      content: sevensContentString,
+      maxWidth: 500
+    });
+
+    //Add Marker
+    var SevensMarker = new google.maps.Marker({
+      position: SevensLatLng,
+      map: map,
+      icon: imagePath,
+      title: 'image title'
+    });
+    
+    google.maps.event.addListener(SevensMarker, 'click', function() {
+      infowindow.open(map,SevensMarker);
+    });
+    
+    
+    
     //Resize Function
     google.maps.event.addDomListener(window, "resize", function() {
       var center = map.getCenter();
